b8a3227f777a763ce615c903a9f79deeb85a1096
Katy DeCorah <decorah@mapbox.com>
2021-03-23 12:24:36 -0400

    Do not transform packages to commonjs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
 'use strict';
 
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false
+      }
+    ],
+    '@babel/preset-react'
+  ],
   plugins: ['@babel/plugin-proposal-class-properties']
 };
