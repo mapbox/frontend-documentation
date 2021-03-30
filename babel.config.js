018caec13ffd4904a48cf26d6654d474ef1fd158
Katy DeCorah <decorah@mapbox.com>
2021-03-30 11:31:25 -0400

    Revert "Update babel.config.js"
    This reverts commit 0bf0fd1c4561c06e0843332fc5d45b253f616f26.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-  presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: ['@babel/plugin-proposal-class-properties']
 };
